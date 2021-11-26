a2ca3825870869dcff0541c22d79cf912e959cde
Rabih Alhousein <rabih.alhousein@gmail.com>
2021-11-26 06:59:46 +0200

    step4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+const showInfo = () => {
+    let surname = document.getElementById("surname").value;
+    let name = document.getElementById("name").value;
+    let city = document.getElementById("city").value;
+    alert ("Nom  : " + name + "\nPrénom  : " + surname + "\nVille  : " + city);
+}
