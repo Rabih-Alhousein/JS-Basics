b7209b60c291298a8723f5c3a686f0cf83b8d437
Rabih Alhousein <rabih.alhousein@gmail.com>
2021-11-26 07:25:50 +0200

    step6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function remainder () {
+    let x = document.getElementById("first_number").value;
+    let y = document.getElementById("second_number").value;
+
+    alert("the remainder is : " + x % y);
+}
