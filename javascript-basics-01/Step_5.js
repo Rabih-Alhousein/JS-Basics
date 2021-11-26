3e194f8490c19092aaddc110bc15cdb3083a36bf
Rabih Alhousein <rabih.alhousein@gmail.com>
2021-11-26 07:22:47 +0200

    step5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function show () {
+    let x = document.getElementById("first_number").value;
+    let y = document.getElementById("second_number").value;
+
+    alert("the ultiplication is : " + x * y);
+}
