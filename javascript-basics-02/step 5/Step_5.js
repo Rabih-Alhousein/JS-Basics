464932088c3a74d3c28e3f59c1c69a539c5ea293
Rabih Alhousein <rabih.alhousein@gmail.com>
2021-11-26 08:48:12 +0200

    step5

---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,26 @@
+var image1 = document.querySelector("#image1"),
+    image2 = document.querySelector("#image2"),
+    image3 = document.querySelector("#image3"),
+    image4 = document.querySelector("#image4"),
+    image5 = document.querySelector("#image5");
+function change() {
+    image1.addEventListener("mouseover", function () {
+        image1.src = "images/image1_2.jpg";
+    });
+
+    image2.addEventListener("mouseover", function () {
+        image2.src = "images/image2_2.jpg";
+    });
+
+    image3.addEventListener("mouseover", function () {
+        image3.src = "images/image3_2.jpg";
+    });
+
+    image4.addEventListener("mouseover", function () {
+        image4.src = "images/image4_2.jpg";
+    });
+
+    image5.addEventListener("mouseover", function () {
+        image5.src = "images/image5_2.jpg";
+    });
+}
