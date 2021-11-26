6ee184fe56a7ed7b16c6d44ac8127d1db9eb707b
Rabih Alhousein <rabih.alhousein@gmail.com>
2021-11-26 08:16:30 +0200

    step1

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,7 @@
+var picture = document.querySelector("#image1");
+picture.addEventListener('mouseover', function(){
+    document.getElementById('image1').src="images/image1_2.jpg";
+})
+picture.addEventListener('mouseout', function(){
+    document.getElementById('image1').src="images/image1.jpg";
+})
