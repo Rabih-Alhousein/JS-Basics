c854ca76d227187f8a6fe055e682a66160f12502
Rabih Alhousein <rabih.alhousein@gmail.com>
2021-11-26 09:11:44 +0200

    step2

--- javascript-basics-03/step 2/Step_2.js
+++ javascript-basics-03/step 2/Step_2.js
@@ -12,23 +12,3 @@ function showhide() {
 }
 
 showhide()
-
-
-
-
-
-/* var show = document.querySelector("#show");
-var hide = document.querySelector("#hide");
-var texte = document.querySelector("#texte");
-
-function showHide(){
-    hide.addEventListener("click", function(){
-        texte.style.display = "none";
-    });
-
-    show.addEventListener("click", function(){
-        texte.style.display = "block";
-    });
-}
-
-showHide(); */
